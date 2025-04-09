46854943b510a196aba55fab59816272f4605ab8
Kyrylo <moisey67kirill@gmail.com>
2025-04-09 18:57:53 +0200

    Added Swagger, URLs for redoc, json-schema, swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Kolekcja API",
+      default_version='v1',
+      description="API do zarządzania kolekcją książek, filmów i gier",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kontakt@twojadomena.pl"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("admin/", admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
