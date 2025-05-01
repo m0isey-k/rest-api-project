f6d810d1e7b799e0e0b5ce025c52a237dd65c311
Dmytro Kharchenko <84476066+dima-kharchenko@users.noreply.github.com>
2025-05-01 17:38:16 +0200

    Dev dima (#19)
    * Add collections pages

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,13 +1,16 @@
 function Sidebar(){
-    const sidebarLinks = ['Favorites', 'Books', 'Films', 'Games']
+    const sidebarLinks = ['Favorites', 'Books', 'Movies']
 
     return(
         <div className="border border-primary-a0 text-surface-a30 select-none rounded-xl mx-4 px-4 py-2">
             {sidebarLinks.map((item, index) => 
+            <a href={`/collection/${item.toLowerCase()}`}>
             <div key={index} className="flex justify-between my-1 hover:text-white transition cursor-pointer">
-                <p key={index}>{item}</p>
-                <i className="fa-solid fa-caret-right"></i>
-            </div>)}
+                    <p key={index}>{item}</p>
+                    <i className="fa-solid fa-caret-right"></i>
+            </div>
+            </a>
+            )}
         </div>
     );
 }
--- frontend/src/Pages/page_routes.jsx
+++ frontend/src/Pages/page_routes.jsx
@@ -4,11 +4,13 @@ import Login from "../Pages/Login";
 import SignUp from "../Pages/SignUp";
 import NotFound from "../Pages/NotFound";
 import Search from "./Search";
+import Collection from "./Collection";
 
 const page_routes = [
   { path: '/', page: Home, protected: true },
   { path: '/search/:term', page: Search, protected: true },
   { path: '/:type/:id', page: Details, protected: true },
+  { path: '/collection/:collection', page: Collection, protected: true },
   { path: '/login', page: Login, protected: false },
   { path: '/signup', page: SignUp, protected: false },
   { path: '*', page: NotFound },  
